23840b0b931dca99c641135e9113849963fac16b
崔建斌 <Richardcjb@163.com>
2017-04-28 10:37:20 +0800

    工具类、entity、form、sql的抽取

--- we-business-parent/we-business-template/we-business-template-provider/src/main/java/com/xiaoka/template/service/impl/TemplateServiceImpl.java
+++ we-business-parent/we-business-template/we-business-template-provider/src/main/java/com/xiaoka/template/service/impl/TemplateServiceImpl.java
@@ -1,6 +1,5 @@
 package com.xiaoka.template.service.impl;
 
-
 import java.util.Map;
 
 import org.nutz.dao.Cnd;
@@ -14,18 +13,18 @@ import com.xiaoka.template.service.entity.TestEntity;
 import com.xiaoka.template.service.form.ParamForm;
 
 @Component(value = "templateService")
-public class TemplateServiceImpl extends BaseService implements TemplateService{
+public class TemplateServiceImpl extends BaseService implements TemplateService {
 
 	@Override
 	public Object queryTestlist(ParamForm queryForm, Pager pager) {
-		Map<String,Object> obj = FormUtil.query(dbDao, TestEntity.class, Cnd.where("name", "=", queryForm.getName()), pager);
-		return obj ;
+		Map<String, Object> obj = FormUtil.query(dbDao, TestEntity.class, Cnd.where("name", "=", queryForm.getName()),
+				pager);
+		return obj;
 	}
 
 	@Override
 	public String sayHello(String name) {
-		return  " Say Hello to " + name; 
+		return " Say Hello to " + name;
 	}
-	
 
 }
